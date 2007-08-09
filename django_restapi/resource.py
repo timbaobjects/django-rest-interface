3ac77734510cf17f009d212825315ff835e46457
Andreas Stuhlmüller <stuhlmueller@gmail.com>
2007-08-09 14:13:26 +0000

    Authentication for generic resource class

--- django_restapi/resource.py
+++ django_restapi/resource.py
@@ -22,51 +22,50 @@ class Resource(object):
     Generic resource class that can be used for
     resources that are not based on Django models.
     """
-    def __init__(self, permitted_methods, mimetype=None):
+    def __init__(self, authentication=None, permitted_methods=None):
         """
+        authentication:
+            the authentication instance that checks whether a
+            request is authenticated
         permitted_methods:
-            the HTTP request methods that are allowed for this
+            the HTTP request methods that are allowed for this 
             resource e.g. ('GET', 'PUT')
-        mimetype:
-            if the default None is not changed, any HttpResponse calls
-            use settings.DEFAULT_CONTENT_TYPE and 
-            settings.DEFAULT_CHARSET
         """
-        self.permitted_methods = [op.upper() for op in permitted_methods]
-        self.mimetype = mimetype
+        # Access restrictions
+        self.authentication = authentication
+        if permitted_methods:
+            self.permitted_methods = [op.upper() for op in permitted_methods]
+        else:
+            self.permitted_methods = ["GET"]
     
-    def get_url_pattern(self, base_url):
-        """
-        Returns an url pattern that redirects any calls to /[base_url]/
-        and /[base_url]/[id]/ indirectly (via the dispatch helper function)
-        to the dispatch method of this resource instance.
-        """
-        return (r'^%s(?:(?P<ident>\d+)/?)?$' % base_url,
-                'django_restapi.resource.dispatch', {'resource' : self})
-    
-    def __call__(self, request):
+    def __call__(self, request, *args, **kwargs):
         """
         Redirects to one of the CRUD methods depending 
         on the HTTP method of the request. Checks whether
         the requested method is allowed for this resource.
         """
+        # Check permission
+        if self.authentication:
+            if not self.authentication.is_authenticated(request):
+                return self.authentication.challenge()
         request_method = request.method.upper()
         if request_method not in self.permitted_methods:
             return HttpResponseNotAllowed(self.permitted_methods)
+        
         if request_method == 'GET':
-            return self.read(request)
+            return self.read(request, *args, **kwargs)
         elif request_method == 'POST':
-            return self.create(request)
+            return self.create(request, *args, **kwargs)
         elif request_method == 'PUT':
             load_put_and_files(request)
-            return self.update(request)
+            return self.update(request, *args, **kwargs)
         elif request_method == 'DELETE':
-            return self.delete(request)
+            return self.delete(request, *args, **kwargs)
         else:
             raise Http404
     
     # The four CRUD methods that any class that 
-    # inherits from Resource may to implement:
+    # inherits from Resource may implement:
     
     def create(self, request):
         raise Http404
